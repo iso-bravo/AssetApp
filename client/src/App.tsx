25a6d8bd9401bdfa45c6910f79206f2a9a32b5d3
iso-bravo <isolettebravo@gmail.com>
2024-03-27 13:20:12 -0700

    Login page name changed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './pages/login';
+import Login from './pages/Login';
 import MainPage from './pages/MainPage';
 import Assets from './pages/Assets';
 

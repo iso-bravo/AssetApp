b7c7676c2db7542a735df6bab8accf16e2740f7d
iso-bravo <isolettebravo@gmail.com>
2024-04-11 20:49:09 -0700

    Changed views

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,15 +3,34 @@ from . import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    
     path('asset_employee_count/', views.AssetAndEmployeesView.as_view(), name='asset_employee_count'),
     path('asset_all/', views.AssetInformationAllView.as_view(), name='asset_all'),
     path('asset_id/', views.AssetInformationByIDView.as_view(), name='asset_id'),
     path('create_asset/', views.CreateAssetView.as_view(), name='create_asset'),
     path('delete_asset/', views.DeleteAssetView.as_view(), name='delete_asset'),
     path('edit_asset/', views.EditAssetView.as_view(), name='edit_asset'),
-    path('users/', views.GetUsersAllView.as_view(), name='users'),
-    path('categories/', views.GetCategoriesAllView.as_view(), name='categories'),
-    path('states/', views.GetStatesAllView.as_view(), name='states'),
-    path('areas/', views.GetAreasAllView.as_view(), name='areas'),
     path('status_categories/', views.GetStatusCategoriesView.as_view(), name='status_categories'),
+    
+    path('users/', views.GetUsersAllView.as_view(), name='users'),
+    path('create_user/', views.CreateUserView.as_view(), name='create_user'),
+    path('edit_user/', views.EditUserByIDView.as_view(), name='edit_user'),
+    path('delete_user/', views.DeleteUserByIDView.as_view(), name='delete_user'),
+    
+    path('categories/', views.GetCategoriesAllView.as_view(), name='categories'),
+    path('create_category/', views.CreateCategoryView.as_view(), name='create_category'),
+    path('edit_category/', views.EditCategoryByIDView.as_view(), name='edit_category'),
+    path('delete_category/', views.DeleteCategoryByIDView.as_view(), name='delete_category'),
+    
+    path('states/', views.GetStatesAllView.as_view(), name='states'),
+    path('create_status/', views.CreateStatusView.as_view(), name='create_status'),
+    path('edit_status/', views.EditStatusByIDView.as_view(), name='edit_status'),
+    path('delete_status/', views.DeleteStatusByIDView.as_view(), name='delete_status'),
+    
+    path('areas/', views.GetAreasAllView.as_view(), name='areas'),
+    path('create_area/', views.CreateAreaView.as_view(), name='create_area'),
+    path('edit_area/', views.EditAreaByIDView.as_view(), name='edit_area'),
+    path('delete_area/', views.DeleteAreaByIDView.as_view(), name='delete_area'),
+    
+    path('export_csv/', views.ExportAssetsCsvView.as_view(), name='export_csv'),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,16 +1,19 @@
 import logging
 from datetime import datetime
+import csv
 
 from django.contrib.auth import login
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework import status as http_status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import (Areas, Asset, Categorias, Departamento, Estados, Permiso, Usuario)
 from .serializers import (AreasSerializer, AssetSerializer, CategoriasSerializer, EstadosSerializer, UsuarioSerializer)
 
-
+#Login
 class LoginView(APIView):
     def post(self, request):
         username = request.data.get('nombre')
@@ -24,7 +27,8 @@ class LoginView(APIView):
             return Response({'mensaje': 'Inicio de sesión exitoso'}, status=200)
         else:
             return Response({'error': 'Credenciales inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
-        
+
+# Home
 class AssetAndEmployeesView(APIView):
     def get(self, request):
         asset_count = Asset.objects.count()
@@ -33,9 +37,10 @@ class AssetAndEmployeesView(APIView):
         data = {'asset_count': asset_count, 'employee_count': employee_count}
         
         return Response(data, status=200)
-        
+
+# Asset
 class AssetInformationAllView(APIView):
-    def get(self):
+    def get(self, request):
         assets = Asset.objects.all()
         serializer = AssetSerializer(assets, many=True)
         
@@ -71,7 +76,7 @@ class AssetInformationByIDView(APIView):
             
             elif field.name == 'id_usuario':
                 user_id = asset.id_usuario
-                if status_id != None and status_id != '':
+                if user_id != None and user_id != '':
                     user = Usuario.objects.get(id=user_id).nombre
                     data[field.name] = user
                 else:
@@ -79,21 +84,23 @@ class AssetInformationByIDView(APIView):
             
             elif field.name == 'id_area':
                 area_id = asset.id_area
-                if status_id != None and status_id != '':
+                if area_id != None and area_id != '':
                     area = Areas.objects.get(id=area_id).area
                     data[field.name] = area
                 else:
                     data[field.name] = ''
+            
+            elif field.name == 'imagen':
+                imagen = asset.imagen
+                data[field.name] = 'api/assets_imgs/' + str(imagen) + '.jpg'
         
             else:
                 data[field.name] = getattr(asset, field.name)
                 
-        logger.error(f'data: {data}')
         return Response(data, status=200)
         
 class CreateAssetView(APIView):
     def post(self, request):
-        logger = logging.getLogger(__name__)
         data = request.data
         new_asset_data = {}
         
@@ -102,7 +109,6 @@ class CreateAssetView(APIView):
                 if field.name == 'id_categoria':
                     category = data[field.name]
                     category_id = Categorias.objects.get(categoria=category).id
-                    logger.error(f'category_id: {category_id}')
                     new_asset_data[field.name] = category_id
                     
                 elif field.name == 'id_estatus':
@@ -112,9 +118,7 @@ class CreateAssetView(APIView):
                     
                 elif field.name == 'id_usuario':
                     user = data[field.name]
-                    logger.error(f'user: {user}')
                     user_id = Usuario.objects.get(nombre=user).id
-                    logger.error(f'user_id: {user_id}')
                     new_asset_data[field.name] = user_id
                     
                 elif field.name == 'id_area':
@@ -175,34 +179,6 @@ class EditAssetView(APIView):
         asset.save()
                 
         return Response({'mensaje': 'Editado correctamente'}, status=200)
-        
-class GetUsersAllView(APIView):
-    def get(self, request):
-        users = Usuario.objects.all()
-        
-        serializer = UsuarioSerializer(users, many=True)
-        return Response(serializer.data, status=200)
-    
-class GetCategoriesAllView(APIView):
-    def get(self, request):
-        categories = Categorias.objects.all()
-        
-        serializer = CategoriasSerializer(categories, many=True)
-        return Response(serializer.data, status=200)
-    
-class GetStatesAllView(APIView):
-    def get(self, request):
-        states = Estados.objects.all()
-        
-        serializer = EstadosSerializer(states, many=True)
-        return Response(serializer.data, status=200)
-
-class GetAreasAllView(APIView):
-    def get(self, request):
-        areas = Areas.objects.all()
-        
-        serializer = AreasSerializer(areas, many=True)
-        return Response(serializer.data, status=200)
     
 class GetStatusCategoriesView(APIView):
     def get(self, request):
@@ -210,10 +186,263 @@ class GetStatusCategoriesView(APIView):
         id_categoria = request.data.get('id_categoria')
         id_estatus = request.data.get('id_estatus')
         
-        category = Categorias.objects.get(id_categoria=id_categoria).categoria
-        status = Estados.objects.get(id_estatus=id_estatus).estatus
+        try:
+            category = Categorias.objects.get(id=id_categoria).categoria
+            status = Estados.objects.get(id=id_estatus).estatus
+            data["categoria"] = category
+            data["estaus"] = status
+            return Response(data, status=200)
+        except ObjectDoesNotExist as e:
+            return Response({'error': str(e)}, status=404)
+        except Exception as e:
+            return Response({'error': str(e)}, status=500)
+
+# Users
+class GetUsersAllView(APIView):
+    def get(self, request):
+        users = Usuario.objects.all()
         
-        data[id_categoria] = category
-        data[id_estatus] = status
+        serializer = UsuarioSerializer(users, many=True)
+        return Response(serializer.data, status=200)
+
+class CreateUserView(APIView):
+    def post(self, request):
+        logger = logging.getLogger(__name__)
+
+        data = request.data
+        logger.error(f'data: {data}')
+        new_user_data = {}
         
-        return Response(data, status=200)
+        for field in Usuario._meta.fields:
+            if field.name in data:
+                if field.name == 'id_departamento':
+                    department = data[field.name]
+                    department_id = Departamento.objects.get(departamento=department).id
+                    new_user_data[field.name] = department_id
+                    
+                elif field.name == 'id_permiso':
+                    permiso = data[field.name]
+                    permiso_id = Permiso.objects.get(permiso=permiso).id
+                    new_user_data[field.name] = permiso_id
+                    
+                else:
+                    new_user_data[field.name] = data[field.name]
+                
+        new_user = Usuario(**new_user_data)
+        new_user.save()
+        
+        return Response({'mensaje': 'Registro exitoso'}, status=200)
+
+class EditUserByIDView(APIView):
+    def post(self, request):
+        data = request.data
+        id_user = request.data.get('id_user')
+        user = Usuario.objects.get(id=id_user)
+        
+        for field in Usuario._meta.fields:
+            if field.name in data:
+                if field.name == 'id_departamento':
+                    department = data[field.name]
+                    department_id = Departamento.objects.get(departamento=department).id
+                    setattr(user, field.name, department_id)
+                    
+                elif field.name == 'id_permiso':
+                    permiso = data[field.name]
+                    permiso_id = Permiso.objects.get(permiso=permiso).id
+                    setattr(user, field.name, permiso_id)
+                    
+                else:
+                    setattr(user, field.name, data[field.name])
+                
+        user.save()
+                
+        return Response({'mensaje': 'Editado correctamente'}, status=200)
+
+class DeleteUserByIDView(APIView):
+    def post(self, request):
+        id_user = request.data.get('id_user')
+        
+        user = Usuario.objects.get(id=id_user)
+        user.delete()
+        
+        return Response({'mensaje': 'Eliminado correctamente'}, status=200)
+
+# Categories
+class GetCategoriesAllView(APIView):
+    def get(self, request):
+        categories = Categorias.objects.all()
+        
+        serializer = CategoriasSerializer(categories, many=True)
+        return Response(serializer.data, status=200)
+    
+class CreateCategoryView(APIView):
+    def post(self, request):
+        data = request.data
+        new_category_data = {}
+        
+        for field in Categorias._meta.fields:
+            if field.name in data:
+                    new_category_data[field.name] = data[field.name]
+                
+        new_category = Categorias(**new_category_data)
+        new_category.save()
+        
+        return Response({'mensaje': 'Registro exitoso'}, status=200)
+
+class EditCategoryByIDView(APIView):
+    def post(self, request):
+        data = request.data
+        id_category = request.data.get('id_category')
+        category = Categorias.objects.get(id=id_category)
+        
+        for field in Categorias._meta.fields:
+            if field.name in data:
+                    setattr(category, field.name, data[field.name])
+                
+        category.save()
+                
+        return Response({'mensaje': 'Editado correctamente'}, status=200)
+
+class DeleteCategoryByIDView(APIView):
+    def post(self, request):
+        id_category = request.data.get('id_category')
+        
+        category = Categorias.objects.get(id=id_category)
+        category.delete()
+        
+        return Response({'mensaje': 'Eliminado correctamente'}, status=200)
+
+# States
+class GetStatesAllView(APIView):
+    def get(self, request):
+        states = Estados.objects.all()
+        
+        serializer = EstadosSerializer(states, many=True)
+        return Response(serializer.data, status=200)
+    
+class CreateStatusView(APIView):
+    def post(self, request):
+        data = request.data
+        new_status_data = {}
+        
+        for field in Estados._meta.fields:
+            if field.name in data:
+                    new_status_data[field.name] = data[field.name]
+                
+        new_status = Estados(**new_status_data)
+        new_status.save()
+        
+        return Response({'mensaje': 'Registro exitoso'}, status=200)
+
+class EditStatusByIDView(APIView):
+    def post(self, request):
+        data = request.data
+        id_status = request.data.get('id_status')
+        status = Estados.objects.get(id=id_status)
+        
+        for field in Estados._meta.fields:
+            if field.name in data:
+                    setattr(status, field.name, data[field.name])
+                
+        status.save()
+                
+        return Response({'mensaje': 'Editado correctamente'}, status=200)
+
+class DeleteStatusByIDView(APIView):
+    def post(self, request):
+        id_status = request.data.get('id_status')
+        
+        status = Estados.objects.get(id=id_status)
+        status.delete()
+        
+        return Response({'mensaje': 'Eliminado correctamente'}, status=200)
+
+# Areas
+class GetAreasAllView(APIView):
+    def get(self, request):
+        areas = Areas.objects.all()
+        
+        serializer = AreasSerializer(areas, many=True)
+        return Response(serializer.data, status=200)
+
+class CreateAreaView(APIView):
+    def post(self, request):
+        data = request.data
+        new_area_data = {}
+        
+        for field in Areas._meta.fields:
+            if field.name in data:
+                    new_area_data[field.name] = data[field.name]
+                
+        new_area = Areas(**new_area_data)
+        new_area.save()
+        
+        return Response({'mensaje': 'Registro exitoso'}, status=200)
+
+class EditAreaByIDView(APIView):
+    def post(self, request):
+        data = request.data
+        id_area = request.data.get('id_area')
+        area = Areas.objects.get(id=id_area)
+        
+        for field in Areas._meta.fields:
+            if field.name in data:
+                    setattr(area, field.name, data[field.name])
+                
+        area.save()
+                
+        return Response({'mensaje': 'Editado correctamente'}, status=200)
+
+class DeleteAreaByIDView(APIView):
+    def post(self, request):
+        id_area = request.data.get('id_area')
+        
+        area = Areas.objects.get(id=id_area)
+        area.delete()
+        
+        return Response({'mensaje': 'Eliminado correctamente'}, status=200)
+
+# Export to csv
+class ExportAssetsCsvView(APIView):
+    def get(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="assets.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['ID', 'Numero de Serie', 'Modelo', 'Descripcion', 'Marca',
+                        'Categoria', 'Fecha de Registro', 'Estatus', 'Tipo de Compra',
+                        'No. Factura/Pedimento', 'Usuario', 'Area'])
+
+        assets = Asset.objects.all()
+
+        for asset in assets:
+            category_id = asset.id_categoria
+            if category_id != None and category_id != '':
+                category = Categorias.objects.get(id=category_id).categoria
+            else:
+                category = ''
+            
+            status_id = asset.id_estatus
+            if status_id != None and status_id != '':
+                status = Estados.objects.get(id=status_id).estatus
+            else:
+                status = ''
+            
+            user_id = asset.id_usuario
+            if user_id != None and user_id != '':
+                user = Usuario.objects.get(id=user_id).nombre
+            else:
+                user = ''
+            
+            area_id = asset.id_area
+            if area_id != None and area_id != '':
+                area = Areas.objects.get(id=area_id).area
+            else:
+                area = ''
+            
+            writer.writerow([asset.id, asset.numero_serie, asset.modelo, asset.descripcion,
+                            asset.marca, category, asset.fecha_registro, status,
+                            asset.tipo_compra, asset.noFactura_pedimento,
+                            user, area])
+
+        return response

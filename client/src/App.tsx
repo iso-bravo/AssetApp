75b084b09e9bd8217f1dc4ad12e0b2c84b352671
iso-bravo <isolettebravo@gmail.com>
2024-04-12 07:33:04 -0700

    Home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
-import MainPage from './pages/MainPage';
 import Assets from './pages/Assets';
 import UsersPage from './pages/Users';
 import CatalogsPage from "./pages/Catalogs";
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<MainPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/assets" element={<Assets />}/>
         <Route path="/users" element={<UsersPage />}/>
         <Route path="/catalogs" element={<CatalogsPage />}/>

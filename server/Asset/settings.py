cae1ad667867392bd4c01183615e0cd6c03bb788
iso-bravo <isolettebravo@gmail.com>
2024-06-03 08:21:09 -0700

    Settings dirs removed

--- server/Asset/settings.py
+++ server/Asset/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Asset.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'labels_templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

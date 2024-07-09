54c902fa4c8f82f6ad3b8491b17d93d0e2eca022
M1keyy <miguel.alcantar@cetys.edu.mx>
2024-07-09 11:16:50 -0700

    Avance catalogos, refactorizacion de componentes

--- client/src/pages/Catalogs.tsx
+++ client/src/pages/Catalogs.tsx
@@ -18,7 +18,7 @@ import {
   GridColDef,
   GridRowSelectionModel,
 } from "@mui/x-data-grid";
-import { useContext, useEffect, useState } from "react";
+import { ReactNode, useContext, useEffect, useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
 import CloseIcon from "@mui/icons-material/Close";
@@ -43,12 +43,21 @@ interface Permiso {
 
 //Componente de la vista de Catálogos
 export default function CatalogsPage() {
+  // hooks para datos de los catálogos
   const [categorias, setCategorias] = useState<GridRowsProp>([]);
   const [estados, setEstados] = useState<GridRowsProp>([]);
   const [areas, setAreas] = useState<GridRowsProp>([]);
+  const [unidadMedida, setUnidadMedida] = useState<GridRowsProp>([]);
+  const [estadosPedimento, setEstadosPedimento] = useState<GridRowsProp>([]);
+
+  // hooks para asignar estado de cargando a los catálogos
   const [loadingCategories, setLoadingCategories] = useState<boolean>(false);
   const [loadingStates, setLoadingStates] = useState<boolean>(false);
   const [loadingAreas, setLoadingAreas] = useState<boolean>(false);
+  const [loadingUnidadMedida, setLoadingUnidadMedida] =
+    useState<boolean>(false);
+  const [loadingEstadosPedimento, setLoadingEstadosPedimento] =
+    useState<boolean>(false);
 
   const [permiso, setPermiso] = useState<String>("viewer");
 
@@ -61,7 +70,7 @@ export default function CatalogsPage() {
   // Usuario autenticado
   const username: String = authContext.user.username;
 
-  // Permisos del usuario autenticado
+  // Obtener permisos de usuario y datos de las tablas
   useEffect(() => {
     API.get("/api/users/").then(async (response) => {
       const usuarios: Usuario[] = response.data;
@@ -85,20 +94,14 @@ export default function CatalogsPage() {
         }
       });
     });
-  }, []);
-
-  useEffect(() => {
     getCategoria();
-  }, []);
-
-  useEffect(() => {
     getEstados();
-  }, []);
-
-  useEffect(() => {
     getAreas();
+    getUnidadMedida();
+    getEstadosPedimento();
   }, []);
 
+  // Obtener data de la base de datos
   function getCategoria() {
     API.get("/api/categories/")
       .then((response) => {
@@ -129,10 +132,37 @@ export default function CatalogsPage() {
       });
   }
 
+  function getUnidadMedida() {
+    API.get("/api/unidad_medida/")
+      .then((response) => {
+        setUnidadMedida(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+      });
+  }
+
+  function getEstadosPedimento() {
+    API.get("/api/estado_pedimento/")
+      .then((response) => {
+        setEstadosPedimento(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+      });
+  }
+
+  // hooks para tener el ID de los catálogos
   const [IDCategoria, setIDCategoria] = useState<GridRowSelectionModel>([-1]);
   const [IDEstado, setIDEstado] = useState<GridRowSelectionModel>([-1]);
   const [IDArea, setIDArea] = useState<GridRowSelectionModel>([-1]);
+  const [IDUnidadMedida, setIDUnidadMedida] = useState<GridRowSelectionModel>([
+    -1,
+  ]);
+  const [IDEstadosPedimento, setIDEstadosPedimento] =
+    useState<GridRowSelectionModel>([-1]);
 
+  // Columnas de los catálogos
   const categoriaCols: GridColDef[] = [
     { field: "id", headerName: "ID", width: 150 },
     { field: "categoria", headerName: "Categoría", width: 400 },
@@ -146,6 +176,20 @@ export default function CatalogsPage() {
   const areasCols: GridColDef[] = [
     { field: "id", headerName: "ID", width: 150 },
     { field: "area", headerName: "Áreas", width: 400 },
+  ];
+
+  const unidadMedidaCols: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 150 },
+    { field: "unidad_medida", headerName: "Unidad de Medida", width: 400 },
+  ];
+
+  const estadosPedimentoCols: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 150 },
+    {
+      field: "estado_pedimento",
+      headerName: "Estado del Pedimento",
+      width: 400,
+    },
   ];
 
   return (
@@ -168,10 +212,21 @@ export default function CatalogsPage() {
               <Box marginBottom={2}>
                 <ButtonGroup>
                   {/* Grupo de Acciones Categoria*/}
-                  <AddCategoryButton
+                  <AddButton
                     ClickHandler={() => getCategoria()}
                     Loading={setLoadingCategories}
-                  />
+                    title={"Añadir Categoría"}
+                    endPoint={"/api/create_category/"}
+                  >
+                    <TextField
+                      label="Nombre"
+                      fullWidth
+                      required
+                      helperText="Escribe el nombre de la categoría."
+                      margin="normal"
+                      name="categoria"
+                    />
+                  </AddButton>
                   <EditCategoryButton
                     ids={IDCategoria}
                     data={categorias}
@@ -185,9 +240,6 @@ export default function CatalogsPage() {
                   />
                 </ButtonGroup>
               </Box>
-              <IconButton onClick={() => getCategoria()}>
-                <ReloadIcon />
-              </IconButton>
               <DataGrid
                 autoHeight
                 rows={categorias}
@@ -207,10 +259,21 @@ export default function CatalogsPage() {
               <Box marginBottom={2}>
                 <ButtonGroup>
                   {/* Grupo de Acciones Estados*/}
-                  <AddStateButton
+                  <AddButton
                     ClickHandler={() => getEstados()}
                     Loading={setLoadingStates}
-                  />
+                    title="Añadir Estado"
+                    endPoint="/api/create_status/"
+                  >
+                    <TextField
+                      label="Nombre"
+                      fullWidth
+                      required
+                      helperText="Escribe el nombre del estado."
+                      margin="normal"
+                      name="estatus"
+                    />
+                  </AddButton>
                   <EditStateButton
                     ids={IDEstado}
                     data={estados}
@@ -224,9 +287,7 @@ export default function CatalogsPage() {
                   />
                 </ButtonGroup>
               </Box>
-              <IconButton onClick={() => getEstados()}>
-                <ReloadIcon />
-              </IconButton>
+
               <DataGrid
                 autoHeight
                 rows={estados}
@@ -246,10 +307,21 @@ export default function CatalogsPage() {
               <Box marginBottom={2}>
                 <ButtonGroup>
                   {/* Grupo de Acciones Areas*/}
-                  <AddAreaButton
+                  <AddButton
                     ClickHandler={() => getAreas()}
                     Loading={setLoadingAreas}
-                  />
+                    title="Añadir Área"
+                    endPoint="/api/create_area/"
+                  >
+                    <TextField
+                      label="Nombre"
+                      fullWidth
+                      required
+                      helperText="Escribe el nombre del área."
+                      margin="normal"
+                      name="area"
+                    />
+                  </AddButton>
                   <EditAreaButton
                     ids={IDArea}
                     data={areas}
@@ -263,9 +335,7 @@ export default function CatalogsPage() {
                   />
                 </ButtonGroup>
               </Box>
-              <IconButton onClick={() => getAreas()}>
-                <ReloadIcon />
-              </IconButton>
+
               <DataGrid
                 autoHeight
                 rows={areas}
@@ -276,6 +346,52 @@ export default function CatalogsPage() {
                 onRowSelectionModelChange={(id) => {
                   const selected: GridRowSelectionModel = id;
                   setIDArea(selected);
+                }}
+              />
+              <Typography variant="h4" className=" py-4">
+                Unidades de Medida
+              </Typography>
+              <Box marginBottom={2}>
+                <ButtonGroup>
+                  {/* Grupo de Acciones Unidad Medida*/}
+                  <AddButton
+                    ClickHandler={() => getUnidadMedida()}
+                    Loading={setLoadingUnidadMedida}
+                    title={"Añadir Unidad de Medida"}
+                    endPoint={"/api/create_unidad_medida/"}
+                  >
+                    <TextField
+                      label="Nombre"
+                      fullWidth
+                      required
+                      helperText="Escribe la unidad de medida nueva."
+                      margin="normal"
+                      name="unidad_medida"
+                    />
+                  </AddButton>
+                  <EditCategoryButton
+                    ids={IDCategoria}
+                    data={categorias}
+                    ClickHandler={() => getCategoria()}
+                    Loading={setLoadingCategories}
+                  />
+                  <DeleteCategoryButton
+                    ids={IDCategoria}
+                    ClickHandler={() => getCategoria()}
+                    Loading={setLoadingCategories}
+                  />
+                </ButtonGroup>
+              </Box>
+              <DataGrid
+                autoHeight
+                rows={unidadMedida}
+                columns={unidadMedidaCols}
+                checkboxSelection
+                loading={loadingUnidadMedida}
+                disableMultipleRowSelection
+                onRowSelectionModelChange={(id) => {
+                  const selected: GridRowSelectionModel = id;
+                  setIDUnidadMedida(selected);
                 }}
               />
             </>
@@ -291,8 +407,11 @@ export default function CatalogsPage() {
 }
 
 interface resetInterface {
-  ClickHandler: Function; //(event: React.MouseEvent<HTMLButtonElement>) => void;
+  ClickHandler: Function;
   Loading: Function;
+  children?: ReactNode;
+  title?: string;
+  endPoint?: string;
 }
 
 interface IDProps extends resetInterface {
@@ -300,7 +419,7 @@ interface IDProps extends resetInterface {
   data?: GridRowsProp;
 }
 
-function AddCategoryButton(reset: resetInterface) {
+function AddButton(reset: resetInterface) {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -333,16 +452,12 @@ function AddCategoryButton(reset: resetInterface) {
             const formData = new FormData(event.currentTarget);
             const formJson = Object.fromEntries((formData as any).entries());
 
-            // TEST
-            //console.log(formJson);
             reset.Loading(true);
-            API.post("/api/create_category/", formJson).then((response) => {
+            API.post(reset.endPoint ?? "", formJson).then((response) => {
               console.log(response);
               reset.ClickHandler();
               reset.Loading(false);
             });
-            //reset.ClickHandler; // Aún no se actualizan las categorías automáticamente => Investigar
-            // Probablemente es porq el reset se hace antes de que la petición POST se termine
             handleClose();
           },
         }}
@@ -360,20 +475,11 @@ function AddCategoryButton(reset: resetInterface) {
           <CloseIcon />
         </IconButton>
         <DialogTitle style={{ backgroundColor: "steelblue" }} color="white">
-          Añadir Categoría
+          {reset.title}
         </DialogTitle>
         <DialogContent draggable>
           {/* Contenido */}
-          <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-            <TextField
-              label="Nombre"
-              fullWidth
-              required
-              helperText="Escribe el nombre de la categoría."
-              margin="normal"
-              name="categoria"
-            />
-          </Box>
+          <Box sx={{ display: "flex", flexWrap: "wrap" }}>{reset.children}</Box>
         </DialogContent>
         <DialogActions style={{ marginBottom: 3, marginRight: 5 }}>
           <Button type="submit" title="Enviar" variant="contained">
@@ -643,101 +749,6 @@ function DeleteCategoryButton(props: IDProps) {
   );
 }
 
-function AddStateButton(reset: resetInterface) {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <>
-      <Button
-        variant="contained"
-        onClick={handleClickOpen}
-        color="success"
-        endIcon={<AddIcon />}
-      >
-        Añadir
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        scroll="paper"
-        fullWidth
-        PaperProps={{
-          component: "form",
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries((formData as any).entries());
-
-            // TEST
-            console.log(formJson);
-
-            reset.Loading(true);
-            API.post("/api/create_status/", formJson).then((response) => {
-              console.log(response);
-              reset.ClickHandler(); // Nop
-              reset.Loading(false);
-            });
-            //reset.ClickHandler();
-            //reset.ClickHandler; // Aún no se actualizan las categorías automáticamente => Investigar
-            // Probablemente es porq el reset se hace antes de que la petición POST se termine
-            handleClose();
-          },
-        }}
-      >
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: "white",
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogTitle style={{ backgroundColor: "steelblue" }} color="white">
-          Añadir Estado
-        </DialogTitle>
-        <DialogContent draggable>
-          {/* Contenido */}
-          <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-            <TextField
-              label="Nombre"
-              fullWidth
-              required
-              helperText="Escribe el nombre del estado."
-              margin="normal"
-              name="estatus"
-            />
-          </Box>
-        </DialogContent>
-        <DialogActions style={{ marginBottom: 3, marginRight: 5 }}>
-          <Button type="submit" title="Enviar" variant="contained">
-            Enviar
-          </Button>
-          <Button
-            title="Cancelar"
-            onClick={handleClose}
-            variant="contained"
-            color="error"
-          >
-            Cancelar
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  );
-}
-
 function EditStateButton(props: IDProps) {
   const [open, setOpen] = React.useState(false);
   const id: number = +props.ids[0];
@@ -983,100 +994,6 @@ function DeleteStateButton(props: IDProps) {
               </Button>
             </>
           )}
-        </DialogActions>
-      </Dialog>
-    </>
-  );
-}
-
-function AddAreaButton(reset: resetInterface) {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <>
-      <Button
-        variant="contained"
-        onClick={handleClickOpen}
-        color="success"
-        endIcon={<AddIcon />}
-      >
-        Añadir
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        scroll="paper"
-        fullWidth
-        PaperProps={{
-          component: "form",
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries((formData as any).entries());
-
-            // TEST
-            console.log(formJson);
-
-            reset.Loading(true);
-            API.post("/api/create_area/", formJson).then((response) => {
-              console.log(response);
-              reset.ClickHandler(); // Nop
-              reset.Loading(false);
-            });
-            //reset.ClickHandler(); // Aún no se actualizan las categorías automáticamente => Investigar
-            // Probablemente es porq el reset se hace antes de que la petición POST se termine
-            handleClose();
-          },
-        }}
-      >
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: "white",
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogTitle style={{ backgroundColor: "steelblue" }} color="white">
-          Añadir Área
-        </DialogTitle>
-        <DialogContent draggable>
-          {/* Contenido */}
-          <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-            <TextField
-              label="Nombre"
-              fullWidth
-              required
-              helperText="Escribe el nombre del área."
-              margin="normal"
-              name="area"
-            />
-          </Box>
-        </DialogContent>
-        <DialogActions style={{ marginBottom: 3, marginRight: 5 }}>
-          <Button type="submit" title="Enviar" variant="contained">
-            Enviar
-          </Button>
-          <Button
-            title="Cancelar"
-            onClick={handleClose}
-            variant="contained"
-            color="error"
-          >
-            Cancelar
-          </Button>
         </DialogActions>
       </Dialog>
     </>
